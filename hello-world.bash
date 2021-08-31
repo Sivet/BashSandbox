cad95427f7ea4382119336043a408e739ff98f28
Sivet <simon@glashoff.dk>
2021-08-31 13:03:19 +0200

    Script now able to be run and sourced

--- hello-world.bash
+++ hello-world.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+main() {
+    hello_world
+}
+
 hello_world() {
     echo "hello, world!"
 }
+
+#If sourced return out
+[[ $FUNCNAME == source ]] && return
+
+#If executed, run main
+main "$@"
